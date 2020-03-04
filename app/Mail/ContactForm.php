c150be5b120d6b7ec6b41fd3d1cb8e46ac2dc5f5
Juan Vargas <juanmvg2006@gmail.com>
2020-03-04 20:19:05 -0300

    Update again this shit

--- app/Mail/ContactForm.php
+++ app/Mail/ContactForm.php
@@ -29,7 +29,7 @@ class ContactForm extends Mailable
      */
     public function build()
     {
-        return $this->from('noreply@tresdear.com')
+        return $this->from('noreply@tresdear.es')
             ->subject('¡Nuevo mensaje de contacto recibido!')
             ->with(['data' => $this->data])
             ->markdown('mails.contactForm');
